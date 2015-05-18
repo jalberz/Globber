c5add7486194e7ca8808f26fb98470f51063c9b1
Jacob Albers <jacobleealbers@gmail.com>
2015-05-18 18:00:18 -0500

    second push

--- Globber.hs
+++ Globber.hs
@@ -1,6 +1,8 @@
 -- Globber.hs
 module Globber (matchGlob) where
-  
+
+import Distribution.Simple 
+
 type GlobPattern = String
 
 matchGlob :: GlobPattern -> String -> Bool
--- TestGlobber.hs
+++ TestGlobber.hs
@@ -7,6 +7,10 @@ import Test.Hspec
 {- locally defined dependencies -}
 import Globber 
 
+import Distribution.Simple
+
 main :: IO ()
-main = do 
-	putStrLn "Hello"
+  main = hspec $
+    describe "empty pattern" $ do
+      it "matches empty string" $
+        matchGlob "" "" `shouldBe` True
