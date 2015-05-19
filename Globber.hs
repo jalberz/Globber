f6cc41326501291c3cc61dc5acd5c775b3c9c316
Jacob Albers <jacobleealbers@gmail.com>
2015-05-18 23:27:28 -0500

    implemented globbing tests

--- Globber.hs
+++ Globber.hs
@@ -1,9 +1,19 @@
 -- Globber.hs
 module Globber (matchGlob) where
 
-import Distribution.Simple 
 
 type GlobPattern = String
 
 matchGlob :: GlobPattern -> String -> Bool
-matchGlob _ _ = True
+matchGlob ('?':xs) (_:ys) = matchGlob xs ys
+matchGlob ("*") (ys) = True
+matchGlob xs@(x:xs') (ys) = 
+	case x of
+		'\\' -> xs' == ys
+		_   -> xs == ys
+matchGlob xs ys = xs == ys
+
+
+maybeHead :: String -> Maybe Char
+maybeHead (x:_) = Just x
+maybeHead [] = Nothing
--- TestGlobber.hs
+++ TestGlobber.hs
@@ -7,10 +7,24 @@ import Test.Hspec
 {- locally defined dependencies -}
 import Globber 
 
-import Distribution.Simple
-
 main :: IO ()
-  main = hspec $
-    describe "empty pattern" $ do
-      it "matches empty string" $
-        matchGlob "" "" `shouldBe` True
+main = hspec $ do
+	describe "Glob test" $ do
+		describe "empty pattern" $ do
+			it "matches empty string" $ 
+				matchGlob "" "" `shouldBe` True
+			it "shouldn't match non-empty string" $ 
+				matchGlob "" "string" `shouldBe` False
+		describe "question mark cases" $ do 
+			it "matches any single character" $ 
+				matchGlob "?" "*" `shouldBe` True
+			it "matches any character within a longer string" $ 
+				matchGlob "?s" "xs" `shouldBe` True
+		describe "star cases" $ do
+			it "matches any string" $
+				matchGlob "*" "xs" `shouldBe` True
+			it "matches empty string" $
+				matchGlob "*" "" `shouldBe` True
+		describe "escaped character cases" $ do
+			it "matches that particular character" $ do
+				matchGlob "\\s" "s" `shouldBe` True
