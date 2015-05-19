8a716e068fb59e7e3966e0e4d4ceb3f0f49e9206
Jacob Albers <jacobleealbers@gmail.com>
2015-05-19 01:16:32 -0500

    All tests passing

--- Globber.hs
+++ Globber.hs
@@ -1,23 +1,63 @@
 -- Globber.hs
-module Globber (matchGlob, matchLiteral) where
+module Globber (matchGlob, matchLiteral, matchLiteralEscape) where
 
 
 type GlobPattern = String
 
+
+--Part 1
 matchGlob :: GlobPattern -> String -> Bool
 matchGlob ('?':xs) (_:ys) = matchGlob xs ys
+--accept anything for '*'
 matchGlob ("*") (_) = True
-matchGlob xs@(x:xs') (ys) = 
-	case x of
-		'\\' -> xs' == ys
-		_   -> xs == ys
+--check for '\', elide it
+matchGlob ('\\':xs') (ys) = xs' == ys
 matchGlob xs ys = xs == ys
 
+--Part 2
 matchLiteral :: GlobPattern -> String -> Bool
-matchLiteral (x:xs') ys@(y:ys') 
+matchLiteral (x:xs') ys@(y:ys')
+	--catch special symbols
 	| (x == '\\' || x == '*' || x == '?' || x == '[') =
+		--elide them
 		matchLiteral xs' ys
+		--otherwise continue matching
 	| otherwise = (x == y) && matchLiteral xs' ys'
 matchLiteral [] []  = True
 matchLiteral _ _ = False
+
+--Part 3
+matchLiteralEscape :: GlobPattern -> String -> Bool
+matchLiteralEscape xs@(x:xs') ys@(_:ys')
+	--check for special symbols
+	| (x == '\\' || x == '*' || x == '?' || x == '[' || x == ']') =
+		--test overall strings
+		xs == ys
+		--remove special and other front char and check
+		|| xs' == ys'
+		--check remainder with entirety of comparison list
+		|| matchLiteralEscape xs' ys
+		--check remainder with tail of comparison list
+		|| matchLiteralEscape xs' ys'
+	|otherwise =
+		-- check for overall match and continue
+		xs == ys || matchLiteralEscape xs' ys'
+matchLiteralEscape [] [] = True
+matchLiteralEscape _ _ = False
+
+
+{-
+Test output:
+reprocessing library globber-0.0.1...
+[1 of 1] Compiling Globber          ( Globber.hs, dist/build/Globber.o )
+In-place registering globber-0.0.1...
+Preprocessing test suite 'test-globber' for globber-0.0.1...
+[1 of 2] Compiling Globber          ( Globber.hs, dist/build/test-globber/test-globber-tmp/Globber.o )
+Linking dist/build/test-globber/test-globber ...
+Running 1 test suites...
+Test suite test-globber: RUNNING...
+Test suite test-globber: PASS
+Test suite logged to: dist/test/globber-0.0.1-test-globber.log
+1 of 1 test suites (1 of 1 test cases) passed.
+-}
 
--- TestGlobber.hs
+++ TestGlobber.hs
@@ -1,3 +1,10 @@
+{-
+Jacob Albers
+CMSC 22311 - Functional Systems in Haskell
+Instructors: Stuart Kurtz & Jakub Tucholski
+2015-05-19
+-}
+
 -- TestGlobber.hs
 module Main (main) where
 
@@ -9,7 +16,8 @@ import Globber
 
 main :: IO ()
 main = hspec $ do
-	describe "Glob test" $ do
+	describe "Glob tests" $ do
+		--Part 1 : Glob Matching
 		describe "empty pattern" $ do
 			it "matches empty string" $ 
 				matchGlob "" "" `shouldBe` True
@@ -28,6 +36,21 @@ main = hspec $ do
 		describe "escaped character cases" $ do
 			it "matches that particular character" $ do
 				matchGlob "\\s" "s" `shouldBe` True
-		describe "literal matching" $ do
+		--Part 2 : Literal Matching
+		describe "literal matching cases" $ do
 			it "treats all non-special characters as literal" $ do
 				matchLiteral "\\*?[s" "s" `shouldBe` True
+		--Part 3 : Literal Escaping
+		describe "literal escape cases" $ do
+			it "escapes backslashes" $ do
+				matchLiteralEscape "\\[a]" "[a]" `shouldBe` True
+			it "elides multiple backslashes" $ do
+				matchLiteralEscape "\\*\\*\\?" "**?" `shouldBe` True
+			it "collapses layers of backslashes" $ do
+				matchLiteralEscape "\\\\a\\\\" "\\a\\" `shouldBe` True
+			it "elides interior backslashes" $ do
+				matchLiteralEscape "ab\\*ba" "ab*ba" `shouldBe` True
+			it "collapses brackets" $ do
+				matchLiteralEscape "ab[a\\]]ba" "ababa" `shouldBe` True
+			it "can collapse brackets another way" $ do
+				matchLiteralEscape "ab[a\\]]ba" "aba]ba" `shouldBe` True
